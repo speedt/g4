06e671e49ebb0c8f54f8c48cd4f555fedbc9da57
speedt <13837186852@qq.com>
2017-09-18 14:28:42 +0800

    haha

---
+++ test/biz/user/getByChannelId.js
@@ -0,0 +1,22 @@
+/*!
+ * test.emag.biz
+ * Copyright(c) 2017 huangxin <3203317@qq.com>
+ * MIT Licensed
+ */
+'use strict';
+
+const assert = require('assert');
+const biz    = require('emag.biz');
+
+describe('biz.user', function(){
+  it('#getByChannelId', function(){
+
+    return biz.user.getByChannelId('1', '2')
+    .then(doc => {
+      assert.equal('1', doc.id);
+    })
+    .catch(err => {
+      assert.ok(!err);
+    })
+  });
+});
